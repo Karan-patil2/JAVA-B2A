252ce181776bfceea80d5b74f3edf3650894c33a
Karan-patil2 <karanpatil9169@gmail.com>
2025-01-30 13:25:08 +0530

    Add files via upload

---
+++ Variable.java
@@ -0,0 +1,16 @@
+//Variable - it is basically used to store information in computer , it can be character , string etc. As the name suggest the value or information of variable can vary.
+//Variable has a data type assign to it like int , float , double etc. 
+//NOTE - In Jshell we can assign or declare multiple values to the  a variable , but in other editor like vs that is not possible to re-declare a variable because its value is already store in the computer memory.
+// let's write a code to understand it .
+
+public class Variable{
+    public static void main(String[] args) {
+        int A = 4; // now if you try to assign a different value to A it will throw a error.
+        System.out.println(A);
+
+        int B = 10 + 2;
+        System.out.println(B);   
+    }
+}
+
+// Comman mistake i make is to save to the code and directly run it , it will also show error , so don't forget to save your code by Ctrl + S.
