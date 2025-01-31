7f7edd2a84842acfb0c9445100b4fb76d9ccec94
KaranByte !! <karanpatil9169@gmail.com>
2025-01-31 23:16:21 +0530

    Add files via upload

---
+++ primitive.java
@@ -0,0 +1,20 @@
+//Primitive data types - int , float , double , decimal , boolean , char.
+//Wrapper class - basically these are the primitive data class but in full form (ex. int is a primitive data type and it's wrapper class is Integer that's the difference). 
+
+
+
+
+
+public class primitive{
+    public static void main(String[] args){
+        int A = Integer.MAX_VALUE;
+        int B = Integer.MIN_VALUE;
+
+        System.out.println("INTEGER RANGE IS = "+Integer.MAX_VALUE+" to "+Integer.MIN_VALUE);
+    }
+}
+
+//this code is basic code through which we understand the primitive data types and their range using variable
+
+//Output
+//INTEGER RANGE IS = 2147483647 to -2147483648
