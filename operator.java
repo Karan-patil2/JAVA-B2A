fc3d7212525c454342809585b699226c2728debe
KaranByte !! <karanpatil9169@gmail.com>
2025-02-01 22:09:39 +0530

    Add files via upload

---
+++ operator.java
@@ -0,0 +1,21 @@
+//In this session , we are going to leran about operators which are +,-,/,= etc .
+
+public class operator{
+    public static void main(String[] args) {
+        int R = 10;
+        R++;//this is called as the post-fix increment operator which is used to increment the value. R-- can be used to decrement the value.
+        System.out.println(R); 
+
+        int Y = 1;
+        Y += 2;
+        System.out.println(Y);//This is called as the addition compound operator.
+
+    }
+}
+
+//Note : "=" is called as assignment operator.
+
+//Output :-
+
+//11
+//3
