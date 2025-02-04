8a689926799b2920853d06e5dc3667e97b9e931f
KaranByte !! <karanpatil9169@gmail.com>
2025-02-04 08:21:23 +0530

    Add files via upload

---
+++ ternary.java
@@ -0,0 +1,9 @@
+public class ternary {
+    public static void main(String[] args) {
+        int age = 20;
+        String A = age >= 18 ? "age is over 18" : "Still a kid";// if age is greater than 18 than it will print 1st statement orelse 2nd statement.
+        System.out.print(A);
+    }
+}
+// ternary operator  : format Op1 ? Op2 : Op3;
+//It is like if-else statement , we can either print this or that .
