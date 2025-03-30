a12702a5921d273401d5c4e02ba7f1d4d35ed8e7
KaranByte !! <karanpatil9169@gmail.com>
2025-03-30 22:17:51 +0530

    Add files via upload

---
+++ CONDITIONAL STATEMENTS/ifelse.java
@@ -0,0 +1,12 @@
+public class ifelse {
+    public static void main(String[] args) {
+        int A = 100;
+//if - else statement - it is used to check the  conditions and print accordingly.
+        if (A >=200){//here if condition is greater than equals to 200 , if that condition was true then it will print the if statement directly without checking further.
+            System.out.print("Your Immortal");
+
+        }else {
+            System.out.print("Your are mortal");
+        }
+    }
+}
