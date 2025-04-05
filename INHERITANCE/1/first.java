d77f67ebddad6690e1702d7fb994e7c4dd9887cb
KaranByte !! <karanpatil9169@gmail.com>
2025-04-05 11:53:28 +0530

    Add files via upload

---
+++ INHERITANCE/1/first.java
@@ -0,0 +1,11 @@
+public class first {       //public is a accese modifer
+    public static void main(String[] args) { //you can just type psvm and hit enter it will automatically pritn public static void main
+        System.out.println("Hello World"); //println is used to give space in lines.
+        System.out.println("Hello karry");
+    }
+}
+
+// This code is being executed in the intelliJ Idea
+//You can download it from jetbrains.com
+//go to jetbrains.com -> click on developer option -> click on IntelliJ idea -> download community edition. and setup it and your ready to run the java code.
+
